831ce630f7af8aabedaae58102715bb15a58a525
Martin Wilhelmi <mnin@mnin.de>
2019-08-19 14:57:11 +0200

    Add support for PaymentMethods from PR #623

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -23,6 +23,7 @@ module StripeMock
     include StripeMock::RequestHandlers::PaymentIntents
     include StripeMock::RequestHandlers::PaymentMethods
     include StripeMock::RequestHandlers::SetupIntents
+    include StripeMock::RequestHandlers::PaymentMethods
     include StripeMock::RequestHandlers::ExternalAccounts
     include StripeMock::RequestHandlers::Accounts
     include StripeMock::RequestHandlers::Balance
@@ -68,6 +69,7 @@ module StripeMock
       @payment_intents = {}
       @payment_methods = {}
       @setup_intents = {}
+      @payment_methods = {}
       @coupons = {}
       @disputes = Data.mock_disputes(['dp_05RsQX2eZvKYlo2C0FRTGSSA','dp_15RsQX2eZvKYlo2C0ERTYUIA', 'dp_25RsQX2eZvKYlo2C0ZXCVBNM', 'dp_35RsQX2eZvKYlo2C0QAZXSWE', 'dp_45RsQX2eZvKYlo2C0EDCVFRT', 'dp_55RsQX2eZvKYlo2C0OIKLJUY', 'dp_65RsQX2eZvKYlo2C0ASDFGHJ', 'dp_75RsQX2eZvKYlo2C0EDCXSWQ', 'dp_85RsQX2eZvKYlo2C0UJMCDET', 'dp_95RsQX2eZvKYlo2C0EDFRYUI'])
       @events = {}
